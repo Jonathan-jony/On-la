51774888a810a2552ce7f97909e0e8f40f143669
Jul1anPerez <100850734+Jul1anPerez@users.noreply.github.com>
2023-11-14 10:40:24 +0100

    first version of the form

--- Onela/Onela/Form1.cs
+++ Onela/Onela/Form1.cs
@@ -16,5 +16,25 @@ namespace Onela
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_magnifyingGlass_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
