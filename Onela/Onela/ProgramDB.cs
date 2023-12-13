ec10ea52f36afbf5ddab901f2fa9c730afb660db
Jonathan-jony <79204604+Jonathan-jony@users.noreply.github.com>
2023-12-13 08:14:16 +0100

    addfonction finish

--- Onela/Onela/ProgramDB.cs
+++ Onela/Onela/ProgramDB.cs
@@ -5,30 +5,20 @@ namespace Onela
 {
     public class DBConnector
     {
-        //The collection of results
-        private static List<string> customers = new List<string>();
-
         public List<Contact> ExecuteQuerySelect()
         {
             List<Contact> queryResults = new List<Contact>();
 
-            //TODO Improvement - use an external file to store sensitive data
             string connString = "server=localhost;user=root;database=onela;port=3306;password=Pa$$w0rd;";
 
-            //prepare the connection
             MySqlConnection connection = new MySqlConnection(connString);
             connection.Open();
 
-            //prepare the query
-            //TODO Improvement - Using @parameter and value to build the query
-            //TODO Improvement - https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlparametercollection.addwithvalue?view=dotnet-plat-ext-7.0
             string query = "SELECT * FROM contact";
 
-            //set and execute the query 
             MySqlCommand cmd = new MySqlCommand(query, connection);
             cmd.ExecuteNonQuery();
            
-            //retrieve values
             MySqlDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
                 {
@@ -45,19 +35,13 @@ namespace Onela
         }
         public bool ExecuteQueryInsert(Contact contact)
         {
-            //TODO Improvement - use an external file to store sensitive data
             string connString = "server=localhost;user=root;database=onela;port=3306;password=Pa$$w0rd;";
 
-            //prepare the connection
             MySqlConnection connection = new MySqlConnection(connString);
             connection.Open();
 
-            //prepare the query
-            //TODO Improvement - Using @parameter and value to build the query
-            //TODO Improvement - https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlparametercollection.addwithvalue?view=dotnet-plat-ext-7.0
             string query = "INSERT INTO contact (firstname, lastname, phone_number) VALUES ('" + contact.Firstname + "', '" + contact.Lastname + "', '" + contact.Numberphone + "');";
 
-            //set and execute the query 
             MySqlCommand cmd = new MySqlCommand(query, connection);
             cmd.ExecuteNonQuery();
             return true;
